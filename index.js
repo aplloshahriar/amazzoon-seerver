dcacc4a116ab8702215b0291c44a220d5199947d
aplloshahriar <aaronshahriar.as@gmail.com>
2023-07-09 15:25:00 +0600

    cart problem solved

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express=require('express');
+const express = require('express');
 const cors = require('cors');
-const app=express();
+const app = express();
 require('dotenv').config();
-const port= process.env.PORT|| 5000;
+const port = process.env.PORT || 5000;
 
 
 //Middleware
@@ -12,7 +12,8 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { parse } = require('dotenv');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7u3xyjj.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,14 +31,28 @@ async function run() {
     await client.connect();
 
     const productCollection = client.db("amazzonDB").collection('products');
-    
-    app.get('/products',async(req,res)=>{
-        const result=await productCollection.find().toArray();
-        res.send(result);
+
+    app.get('/products', async (req, res) => {
+      console.log(req.query);
+      const page = parseInt(req.query.page) || 0;
+      const limit = parseInt(req.query.limit) || 9;
+      const skip = page * limit;
+      const result = await productCollection.find().skip(skip).limit(limit).toArray();
+      res.send(result);
     })
-    app.get('/totalProducts',async(req,res)=>{
-        const result=await productCollection.estimatedDocumentCount()
-        res.send({ totalProducts:result});
+
+    app.get('/totalProducts', async (req, res) => {
+      const result = await productCollection.estimatedDocumentCount()
+      res.send({ totalProducts: result });
+    })
+
+    app.post('/productsById', async (req, res) => {
+      const storedCartId=req.body;
+      const objectId=storedCartId.map(id=> new ObjectId(id))
+      const query={_id: { $in: objectId}}
+      console.log(storedCartId);
+      const result=await productCollection.find(query).toArray();
+      res.send(result);
     })
 
 
@@ -60,10 +75,10 @@ run().catch(console.dir);
 
 
 
-app.get('/',(req,res)=>{
-    res.send('Ama is busy with spending')
+app.get('/', (req, res) => {
+  res.send('Ama is busy with spending')
 })
 
-app.listen(port,()=>{
-    console.log(`Amazzon server is running on port: ${port}`);
+app.listen(port, () => {
+  console.log(`Amazzon server is running on port: ${port}`);
 })
